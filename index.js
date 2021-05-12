b3f63abcfefb97a31884f12695aa50e1eb72dd8e
Hajin Kim <rhrnak624@naver.com>
2021-05-12 21:12:43 +0900

    1.6 ~ 1.10

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("I'm Working. I'm beautiful. I'm worth it.");
+let hajinInfo = {
+    name: "hajin",
+    age : 18,
+    gender : "Female",
+    isPretty : true,
+    favMovies : [
+        "Twilight",
+        "Harry Potter"
+    ],
+    favFood : [
+        "Maratang",
+        "Galbi",
+        "Tteokbokki"
+    ],
+    favArtist : [
+        {
+            name : "baekhyun",
+            age: 30,
+            job : "singer"
+        },
+        {
+            name: "teo",
+            age : 40,
+            job : "actor"
+        }
+    ]
+}
+
+console.log(hajinInfo.favArtist[1].name);
