636082ac26d092ee014e5a8e84faeba57b6d2bbb
Hajin Kim <rhrnak624@naver.com>
2021-05-13 17:32:04 +0900

    2.1

--- index.js
+++ index.js
@@ -27,3 +27,41 @@ let hajinInfo = {
 }
 
 console.log(hajinInfo.favArtist[1].name);
+
+
+function sayHello(name, age) {
+    console.log(`Hello ${name} you are ${age} years old`);
+}
+
+sayHello("Hajin", 18);
+
+const calculator = {
+    plus : function(a,b) {
+        return a + b;
+    },
+    minus : function(a, b) {
+        return a - b;
+    },
+    multiply : function(a, b) {
+        return a * b;
+    },
+    divide : function(a, b) {
+        return a / b;
+    },
+    power : function(a, b) {
+        return a ** b;
+    }
+};
+
+const plus = calculator.plus(5, 5);
+const minus = calculator.minus(5, 5);
+const multiply = calculator.multiply(5, 5);
+const divide = calculator.divide(5, 5);
+const power = calculator.power(5, 5);
+console.log(plus);
+console.log(minus);
+console.log(multiply);
+console.log(divide);
+console.log(power);
+
+
